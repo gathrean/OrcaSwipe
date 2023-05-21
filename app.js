f9ba3b60cfe3138f7bb2e649a1a248504cde10ba
Gathrean Dela Cruz <gathrean@icloud.com>
2023-05-20 17:36:29 -0700

    Added the feature that shows the user's profile picture in chat (it also shows the other users' pfps)

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ app.use(express.static("public"));
 app.use('/splash', express.static('views/splash'));
 
 // Serve static files from the "/views/chat" directory
-app.use('/chat', express.static('views/chat'));
+app.use('/chat/uploads', express.static('uploads'));
 
 // Serve static files from /uplaods directory for photo rendering
 app.use('/uploads', express.static('uploads'));
