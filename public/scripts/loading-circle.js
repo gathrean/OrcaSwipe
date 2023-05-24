d39ae38f4e6810ed0845142417a20cfa2ab19d42
Gathrean Dela Cruz <gathrean@icloud.com>
2023-05-24 00:31:26 -0700

    Removed loading-circle from findPods and made it its own js and css files

---
+++ public/scripts/loading-circle.js
@@ -0,0 +1,11 @@
+// Function to show the loading screen
+function showLoadingScreen() {
+    var loadingCircle = document.getElementById('loading-circle');
+    loadingCircle.style.display = 'block';
+}
+
+// Function to hide the loading screen
+function hideLoadingScreen() {
+    var loadingCircle = document.getElementById('loading-circle');
+    loadingCircle.style.display = 'none';
+}
