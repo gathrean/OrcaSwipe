f4e118f007560ce6d8989098038a3f328e216a17
AlexSichitiu <asichitiu@gmail.com>
2023-05-24 14:31:44 -0700

    Got ChatGPT to produce an array of interests that is parsed into seperate buttons.

--- public/scripts/openai.js
+++ public/scripts/openai.js
@@ -13,8 +13,8 @@ const generateResponse = async () => {
     //Set loading spinner
     loading.classList.remove("visually-hidden");
     submit.classList.add("visually-hidden");
-    const input = userInput.value;
-    const response = await fetch('/chatgpt', {
+    var input = userInput.value;
+    var response = await fetch('/chatgpt', {
         method: 'POST',
         body: JSON.stringify({
             model: "gpt-3.5-turbo",
@@ -26,8 +26,8 @@ const generateResponse = async () => {
         }
     });
 
-    const responseData = await response.json();
-    const message = responseData.result[0].message.content;
+    var responseData = await response.json();
+    var message = responseData.result[0].message.content;
     console.log(message);
 
     //Store our previous messages
@@ -35,27 +35,45 @@ const generateResponse = async () => {
     //Clear both fields
     userInput.value = "";
 
-    const historyElement = document.createElement('div');
-    historyElement.innerHTML = `<li class="list-group-item">Prompt: ${input}</li>
-    <li class="list-group-item"> Response: ${message}</li>`;
+    var historyElement = document.createElement('div');
+    historyElement.innerHTML = `<li class="list-group-item"><b>You:</b> ${input}</li>
+                                <li class="list-group-item"><b>ChatGPT:</b> ${message}</li>`;
     chatHistory.append(historyElement);
 
     //Stop loading spinner
     loading.classList.add("visually-hidden");
     submit.classList.remove("visually-hidden");
 
+    interests = [];
+    var activitySuggestionPrompt = `Given what I just said, could you generate an array of activities or activity types that might interest me? Ensure that the array is parseable in JavaScript using JSON.parse().`
+    var modifiedInput = input + activitySuggestionPrompt;
+    response = await fetch('/chatgpt', {
+        method: 'POST',
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{"role": "user", "content": modifiedInput}],
+            temp: 0.6
+        }), 
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    responseData = await response.json();
+    message = responseData.result[0].message.content;
+    console.log(message);
+    interests = JSON.parse(message);
+    populateInterests(interests);
 }
 
 //Assign onclick method
 submit.onclick = generateResponse;
 
-const interests = ['Ocean Clean-up', 'Volunteer', 'Charity', 'Black Lives Matter', 'Clothing drive', 'Blood drive', 'Art', 'Cancer Walk', 'Travel', 'Photography'];
+var interests = ['Ocean Clean-up', 'Volunteer', 'Charity', 'Black Lives Matter', 'Clothing drive', 'Blood drive', 'Art', 'Cancer Walk', 'Travel', 'Photography'];
 
-function populateInterests(){
+function populateInterests(interests){
     $('#interests-buttons').empty();
     for (var i = 0; i < interests.length; i++){
-        console.log(interests[i]);
         $('#interests-buttons').append(`<button class="interest-button" type="button" data-interest="${interests[i]}">${interests[i]}</button>`);
     }
 }
-populateInterests();
+populateInterests(interests);
