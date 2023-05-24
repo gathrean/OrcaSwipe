8ce0a7b5d9323670dbb85c3cfba963757d1a788b
AlexSichitiu <asichitiu@gmail.com>
2023-05-24 15:14:37 -0700

    Removed initial chatGPT repsonse and got chatGPT to send a list of activities as an array and openai.js parses it and renders corresponding buttons.

--- public/scripts/openai.js
+++ public/scripts/openai.js
@@ -3,6 +3,10 @@ const submit = document.getElementById("submit");
 const userInput = document.getElementById("user-input");
 const chatHistory = document.getElementById("chat-history");
 const loading = document.getElementById("spinner");
+const buttons = document.getElementById('interests-buttons');
+const buttonsLoading = document.getElementById("spinner2");
+const set = document.getElementById('set-interests');
+const interestDiv = document.getElementById('interests-buttons');
 
 let promptResponses = [];
 
@@ -13,22 +17,24 @@ const generateResponse = async () => {
     //Set loading spinner
     loading.classList.remove("visually-hidden");
     submit.classList.add("visually-hidden");
+    buttonsLoading.classList.remove("visually-hidden");
+    buttons.classList.add("visually-hidden");
+    set.classList.add("visually-hidden");
     var input = userInput.value;
-    var response = await fetch('/chatgpt', {
-        method: 'POST',
-        body: JSON.stringify({
-            model: "gpt-3.5-turbo",
-            messages: [{"role": "user", "content": input}],
-            temp: 0.6
-        }), 
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
+    // var response = await fetch('/chatgpt', {
+    //     method: 'POST',
+    //     body: JSON.stringify({
+    //         model: "gpt-3.5-turbo",
+    //         messages: [{"role": "user", "content": input}],
+    //         temp: 0.6
+    //     }), 
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     }
+    // });
 
-    var responseData = await response.json();
-    var message = responseData.result[0].message.content;
-    console.log(message);
+    // var responseData = await response.json();
+    // var message = responseData.result[0].message.content;
 
     //Store our previous messages
     promptResponses.push({question: input, response: message});
@@ -36,8 +42,7 @@ const generateResponse = async () => {
     userInput.value = "";
 
     var historyElement = document.createElement('div');
-    historyElement.innerHTML = `<li class="list-group-item"><b>You:</b> ${input}</li>
-                                <li class="list-group-item"><b>ChatGPT:</b> ${message}</li>`;
+    historyElement.innerHTML = `<li class="list-group-item"><b>You:</b> ${input}</li>`;
     chatHistory.append(historyElement);
 
     //Stop loading spinner
@@ -45,35 +50,48 @@ const generateResponse = async () => {
     submit.classList.remove("visually-hidden");
 
     interests = [];
-    var activitySuggestionPrompt = `Given what I just said, could you generate an array of activities or activity types that might interest me? Ensure that the array is parseable in JavaScript using JSON.parse().`
+    var activitySuggestionPrompt = `Given what I just said, could you generate an array of activities or activity types that might interest me? 
+        Ensure that the array is parseable in JavaScript and only respond with the array, nothing else.`
     var modifiedInput = input + activitySuggestionPrompt;
-    response = await fetch('/chatgpt', {
-        method: 'POST',
-        body: JSON.stringify({
-            model: "gpt-3.5-turbo",
-            messages: [{"role": "user", "content": modifiedInput}],
-            temp: 0.6
-        }), 
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
-    responseData = await response.json();
-    message = responseData.result[0].message.content;
-    console.log(message);
-    interests = JSON.parse(message);
-    populateInterests(interests);
+    interestDiv.replaceChildren();
+
+    try {
+        var response = await fetch('/chatgpt', {
+            method: 'POST',
+            body: JSON.stringify({
+                model: "gpt-3.5-turbo",
+                messages: [{"role": "user", "content": modifiedInput}],
+                temp: 0.6
+            }), 
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        var responseData = await response.json();
+        var message = responseData.result[0].message.content;
+        console.log(message);
+        interests = JSON.parse(message);
+        populateInterests(interests);
+    } catch (error) {
+        console.log(error)
+        interestDiv.append('Sorry, I couldn\'t generate any suggestions for you. Please try again.');
+    }
+    buttonsLoading.classList.add("visually-hidden");
+    buttons.classList.remove("visually-hidden");
+    set.classList.remove("visually-hidden");
 }
 
 //Assign onclick method
 submit.onclick = generateResponse;
 
-var interests = ['Ocean Clean-up', 'Volunteer', 'Charity', 'Black Lives Matter', 'Clothing drive', 'Blood drive', 'Art', 'Cancer Walk', 'Travel', 'Photography'];
-
 function populateInterests(interests){
-    $('#interests-buttons').empty();
+    interestDiv.replaceChildren();
     for (var i = 0; i < interests.length; i++){
-        $('#interests-buttons').append(`<button class="interest-button" type="button" data-interest="${interests[i]}">${interests[i]}</button>`);
+        var button = document.createElement('button');
+        button.setAttribute('class', 'interest-button');
+        button.setAttribute('data-interest', interests[i])
+        button.setAttribute('type', 'button');
+        button.insertAdjacentText('afterbegin', interests[i]);
+        interestDiv.append(button);
     }
 }
-populateInterests(interests);
