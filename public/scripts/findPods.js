3b6544d705da8e9adf2f6cb55d239865466bca61
Gathrean Dela Cruz <gathrean@icloud.com>
2023-05-24 17:38:09 -0700

    Rotate the behind-cards in the stack by 3 degrees

--- public/scripts/findPods.js
+++ public/scripts/findPods.js
@@ -45,9 +45,8 @@ function initCards(card, index) {
     }
 
     newCards.forEach(function (card, index) {
-        // Setting the stacked effect for each card
         card.style.zIndex = allCards.length - index;
-        card.style.transform = 'scale(' + (20 - index) / 20 + ') translateY(-' + 30 * index + 'px)';
+        card.style.transform = 'scale(' + (20 - index) / 20 + ') translateY(-' + 30 * index + 'px) rotate(' + -3 * index + 'deg)';
         card.style.opacity = (10 - index) / 10;
     });
 
